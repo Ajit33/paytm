0cbba4ddf1eb6b9356ba1ef0ec946698690dba8c
Ajit33 <justajit33@gmail.com>
2024-05-03 01:36:53 +0530

    purpose:- set up the routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />}  />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Send />} />
+      </Routes>
+    </Router>
   )
 }
 
